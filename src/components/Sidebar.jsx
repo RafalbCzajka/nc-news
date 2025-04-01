da5a9cc8e172fc8bbfc4e5a2a537a95de50e3362
RafalbCzajka <rafalbczajka@gmail.com>
2025-04-01 10:39:23 +0100

    Implemented custom react hook for data fetching

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,23 +2,17 @@ import SidebarButton from "./SidebarButton"
 import { useState, useEffect } from "react";
 import { getTopics } from "../../api";
 import { useSidebar } from "../Contexts/SidebarContext";
+import useApiRequest from "../hooks/useApiRequest"
+import Loading from "./Loading";
 
 export default function Sidebar() {
     const {isSidebarOpen} = useSidebar();
-    const [topics, setTopics] = useState([]);
-
-    useEffect(() => {
-        getTopics()
-            .then((topics) => {
-                setTopics(topics);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }, []);
+    const {data: topics, isLoading, error} = useApiRequest(getTopics);
 
     return (
         <aside id="sidebar" className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
+            {isLoading && <Loading/>}
+            {error && <p>{error.msg}</p>}
             <ul>
                 {topics.map((topic) => (
                     <SidebarButton key={topic.slug} topic={topic} />
