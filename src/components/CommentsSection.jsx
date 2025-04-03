a982f94c539c304b2069e410404cd3a96fa42f5d
RafalbCzajka <rafalbczajka@gmail.com>
2025-04-03 13:31:15 +0100

    fixed optimistic rendering when an error occurs

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -6,10 +6,10 @@ import CommentForm from "./CommentForm";
 import { useState, useEffect } from "react";
 import { useLoggedInUser } from "../Contexts/LoggedInUserContext";
 
-export default function CommentsSection({articleId}) {
-    const {data: initialComments, isLoading, error} = useApiRequest(getComments, articleId);
+export default function CommentsSection({ articleId }) {
+    const { data: initialComments, isLoading, error } = useApiRequest(getComments, articleId);
     const [comments, setComments] = useState([]);
-    const {loggedInUser} = useLoggedInUser();
+    const { loggedInUser } = useLoggedInUser();
 
     useEffect(() => {
         if (initialComments) {
@@ -18,29 +18,31 @@ export default function CommentsSection({articleId}) {
     }, [initialComments])
 
 
-    const handleDelete = (commentId) => {
-        if(window.confirm("Are you sure you want to delete this comment?")) {
-            setComments((prevComments) => prevComments.filter(comment => comment.comment_id !== commentId));
+    const handleDelete = async (commentId) => {
+        if (!window.confirm("Are you sure you want to delete this comment?")) {
+            return;
+        }
+        const previousComments = [...comments];
+        setComments((prevComments) => prevComments.filter(comment => comment.comment_id !== commentId));
 
-            deleteComment(commentId).then(() => {
-                console.log("comment deleted successfully");
-            })
-            .catch((err) => {
-                setComments((prevComments) => [...prevComments]);
-                alert("Error deleting comment, please try again.");
-            }) 
+        try {
+            await deleteComment(commentId);
+        } catch (err) {
+            console.log(err);
+            alert("Error deleting comment, please try again.");
+            setComments(previousComments);
         }
     }
 
-    return(
+    return (
         <section>
-            <CommentForm articleId={articleId} setComments={setComments}/>
+            <CommentForm articleId={articleId} setComments={setComments} />
             <h4>Comments:</h4>
-            {isLoading && <Loading/>}
+            {isLoading && <Loading />}
             {error && <p>{error.msg}</p>}
             <ul id="comments-list">
                 {comments.map((comment) => (
-                    <CommentCard key={comment.comment_id} comment={comment} canDelete={comment.author === loggedInUser} onDelete={() => handleDelete(comment.comment_id)}/>
+                    <CommentCard key={comment.comment_id} comment={comment} canDelete={comment.author === loggedInUser} onDelete={() => handleDelete(comment.comment_id)} />
                 ))}
             </ul>
         </section>
