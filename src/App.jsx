23ce841fc54d56a82fb374fc7b6339c7d79dc16e
RafalbCzajka <rafalbczajka@gmail.com>
2025-04-04 14:57:00 +0100

    Implemented view for invalid path and made CSS adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router"
+import { BrowserRouter, Route, Routes} from "react-router"
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import ArticleListView from './components/ArticleListView'
 import SingleArticleView from './components/SingleArticleView'
+import NotFoundPage from './components/NotFoundPage'
 
 function App() {
 
   return (
     <BrowserRouter>
-    <Header/>
-    <Sidebar/>
-    <Routes>
-      <Route path="/" element={<ArticleListView/>}/>
-      <Route path="/articles/:article_id" element={<SingleArticleView/>}/>
-    </Routes>
+      <Header />
+      <Sidebar />
+        <Routes>
+          <Route path="/" element={<ArticleListView />} />
+          <Route path="/articles/:article_id" element={<SingleArticleView />} />
+          <Route path="*" element={<NotFoundPage/>} />
+        </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+        <section className="not-found">
+            <h2>Page not found.</h2>
+        </section>
+    )
+}
