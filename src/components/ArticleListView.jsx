4be825b3b5fd8af9c70c6baa4035aef7b9be9a22
RafalbCzajka <rafalbczajka@gmail.com>
2025-04-04 10:54:15 +0100

    Implemented filter bar component

--- src/components/ArticleListView.jsx
+++ src/components/ArticleListView.jsx
@@ -1,4 +1,5 @@
 import ArticleList from "./ArticleList"
+import FilterBar from "./FilterBar";
 import { useSearchParams } from "react-router"
 
 export default function ArticleListView() {
@@ -8,6 +9,7 @@ export default function ArticleListView() {
     return(
         <main className="content">
             <h2>{topic ? `Articles about ${topic}` : "All Articles"}</h2>
+            <FilterBar/>
             <ArticleList/>
         </main>
     )
