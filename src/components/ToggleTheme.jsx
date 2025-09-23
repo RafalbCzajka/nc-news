23c7c71d65700f246fcfc8b8df6a4f96829d20f6
RafalbCzajka <rafalbczajka@gmail.com>
2025-09-23 19:09:37 +0100

    Updated CSS for header h1 and toggle theme button to stand out more

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -4,8 +4,8 @@ export default function ToggleTheme() {
     const {isDarkTheme, setTheme} = useTheme();
     return (
         <div id="toggle-theme">
-        <label className="switch-label">
-            <input type="checkbox" className="checkbox" checked={!isDarkTheme} onChange={() => setTheme(prev => !prev)}/>
+        <label className="switch-label" title="Toggle light/dark theme">
+            <input type="checkbox" className="checkbox"  aria-label="Toggle light or dark theme" checked={!isDarkTheme} onChange={() => setTheme(prev => !prev)}/>
                 <span className="slider"></span>
         </label>
     </div>
